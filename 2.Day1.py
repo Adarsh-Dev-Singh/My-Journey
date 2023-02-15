3de86e494eb8a5def8a2f8e117202c57b45e6f44
Ansh <jamwalansh987@gmail.com>
2023-02-16 00:24:25 +0530

    Nothing

--- 2.Day1.py
+++ 2.Day1.py
@@ -1,8 +1,8 @@
 import random
-l1 = ['Aadhavi','Aanya','Aarya','Aashvi','Avni','Alia','Amulya','Aniya','Aruhi']
+l1 = ['Aadhavi','Aanya','Aarya','Aastha','Avni','Alia','Amulya','Aniya','Aruhi']
 x=0
 l2= input("Enter Your name\n")
-for i in range(9):
+for i in range(100):
     x=x+1
     v1 = random.choice(l1)
     print(f"{l2} will make out with {v1} on the day  {x}")
